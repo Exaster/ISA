d9ba79092137ff20e32dbad3260febaf0dbf496c
Exaster <dmytrykvaleriy@gmail.com>
2023-11-05 17:37:05 +0300

    added more colors and possiblities for customisation!

--- lib/utils/color_picker.dart
+++ lib/utils/color_picker.dart
@@ -17,32 +17,64 @@ class ColorPicker extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Вибір теми'),
-      content: Column(
-        children: [
-          Text('Основні кольори:'),
-          ColorPickerButton(
-            onColorSelected: onHeaderColorSelected,
-            colors: [
-              hexToColor("#333333"),  // Dark Gray
-              hexToColor("#1A237E"),  // Navy Blue
-              hexToColor("#009688"),  // Teal
-              hexToColor("#FF5733"),  // Dark Orange
-              hexToColor("#673AB7"),  // Deep Purple
-            ],
-          ),
-          SizedBox(height: 16.0),
-          Text('Фоновий колір'),
-          ColorPickerButton(
-            onColorSelected: onBackgroundColorSelected,
-            colors: [
-              hexToColor("#1BF702"),
-              hexToColor("#AA1BF7"),
-              hexToColor("#F7DD11"),
-              hexToColor("#FFEBD6"),
-              hexToColor("#EAE7E9"),
-            ],
-          ),
-        ],
+      content: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('Основні кольори для заголовків:'),
+            ColorPickerButton(
+              onColorSelected: onHeaderColorSelected,
+              colors: [
+                hexToColor("#333333"),  // Dark Gray
+                hexToColor("#1A237E"),  // Navy Blue
+                hexToColor("#009688"),  // Teal
+                hexToColor("#FF5733"),  // Dark Orange
+                hexToColor("#673AB7"),  // Deep Purple
+                hexToColor("#E57373"),  // Light Red
+                hexToColor("#26A69A"),  // Turquoise
+                hexToColor("#FFA726"),  // Orange
+                hexToColor("#03A9F4"),  // Light Blue
+                hexToColor("#4CAF50"),  // Green
+                hexToColor("#FF3D00"),  // Tomato Red
+                hexToColor("#4E342E"),  // Brown
+                hexToColor("#795548"),  // Light Brown
+                hexToColor("#8D6E63"),  // Taupe
+                hexToColor("#00BCD4"),  // Cyan
+                hexToColor("#FF4081"),  // Pink
+                hexToColor("#3F51B5"),  // Indigo
+                hexToColor("#827717"),  // Mustard
+                hexToColor("#9C27B0"),  // Purple
+                hexToColor("#607D8B"),  // Blue Gray
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text('Фонові кольори:'),
+            ColorPickerButton(
+              onColorSelected: onBackgroundColorSelected,
+              colors: [
+                hexToColor("#1BF702"),  // Neon Green
+                hexToColor("#AA1BF7"),  // Lavender
+                hexToColor("#F7DD11"),  // Yellow
+                hexToColor("#FFEBD6"),  // Peach
+                hexToColor("#EAE7E9"),  // Gray
+                hexToColor("#B2EBF2"),  // Light Blue
+                hexToColor("#D1C4E9"),  // Light Purple
+                hexToColor("#C5E1A5"),  // Light Green
+                hexToColor("#FFD600"),  // Bright Yellow
+                hexToColor("#BBDEFB"),  // Sky Blue
+                hexToColor("#FF8A80"),  // Coral
+                hexToColor("#C8E6C9"),  // Light Green
+                hexToColor("#FFE0B2"),  // Light Orange
+                hexToColor("#E0E0E0"),  // Silver
+                hexToColor("#90CAF9"),  // Blue
+                hexToColor("#FF80AB"),  // Pink
+                hexToColor("#7986CB"),  // Indigo
+                hexToColor("#A5D6A7"),  // Green
+                hexToColor("#A1887F"),  // Brown
+                hexToColor("#E0E0E0"),  // Light Gray
+              ],
+            ),
+          ],
+        ),
       ),
       actions: <Widget>[
         TextButton(
@@ -67,29 +99,32 @@ class ColorPickerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      children: [
-        for (final color in colors)
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: GestureDetector(
-              onTap: () => onColorSelected(color),
-              child: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: color,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2.0,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (final color in colors)
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: GestureDetector(
+                onTap: () => onColorSelected(color),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: color,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
